718f810df5b9c24280bba2c263445283089ff248
JiHoParkour <roadcodesoar@gmail.com>
2022-06-18 15:10:10 +0900

    initial commit

---
+++ GiphySearchClone/Sources/Presentation/SearchViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SearchViewController.swift
+//  GiphySearchClone
+//
+//  Created by yupzip on 2022/06/18.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    // MARK: Properties
+    
+    
+    // MARK: View Control
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUpView()
+    }
+    
+    func setUpView() {
+        
+        
+    }
+    
+    // MARK: User Actions
+    
+    
+    // MARK: Helper Methods
+    
+    
+}
