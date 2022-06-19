aae0e9a967fd35d1a3abcfe31e680024afa64412
JiHoParkour <roadcodesoar@gmail.com>
2022-06-20 02:18:22 +0900

    add : gif 상세 화면의 콜렉션뷰에서 쓸 헤더 뷰

---
+++ GiphySearchClone/Sources/Presentation/SearchTab/Search/ViewControllers/CollectionReusableView.swift
@@ -0,0 +1,16 @@
+//
+//  CollectionReusableView.swift
+//  GiphySearchClone
+//
+//  Created by yupzip on 2022/06/19.
+//
+
+import UIKit
+
+class CollectionReusableView: UICollectionReusableView {
+        
+    static let identifier = "CollectionReusableView"
+    @IBOutlet weak var gifImageView: UIImageView!
+    @IBOutlet weak var userProfileImageView: UIImageView!
+    
+}
