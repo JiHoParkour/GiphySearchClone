b5e091d663de24fce6e588075cd2b95eca1b7db7
JiHoParkour <roadcodesoar@gmail.com>
2022-06-20 02:17:38 +0900

    add : gif list 콜렉션뷰에서 쓸 셀

---
+++ GiphySearchClone/Sources/Presentation/SearchTab/Search/Cells/CVGifCell.swift
@@ -0,0 +1,22 @@
+//
+//  CVGifCell.swift
+//  GiphySearchClone
+//
+//  Created by yupzip on 2022/06/19.
+//
+
+import UIKit
+
+class CVGifCell: UICollectionViewCell {
+   
+    @IBOutlet weak var gifImageView: UIImageView!
+    static let identifier = "CVGifCell"
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        gifImageView.layer.cornerRadius = 10
+        contentView.layer.cornerRadius = 10
+
+    }
+}
